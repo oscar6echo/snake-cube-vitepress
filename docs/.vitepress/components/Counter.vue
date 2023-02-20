1ce31ce31492122d62698fd522511a6e4ea9e3fa
Olivier Borderies <olivier.borderies@gmail.com>
2023-02-20 18:04:23 +0100

    Update

--- docs/.vitepress/components/Counter.vue
+++ docs/.vitepress/components/Counter.vue
@@ -1,13 +1,25 @@
 <template>
-  <button @click="increment">Count is: {{ count }}</button>
+  <keep-alive>
+    <button @click="increment">Count is: {{ count }}</button>
+  </keep-alive>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { onMounted, onUnmounted, ref } from "vue";
 
 const count = ref(0);
 
 const increment = () => {
   count.value++;
+  console.log(count.value);
 };
+
+onMounted(() => {
+  console.log("onMounted");
+  console.log(count.value);
+});
+
+onUnmounted(() => {
+  console.log("onUnmounted");
+});
 </script>
